27769240f6723a536809ae55d34e66d86bc737f3
savar19 <3884346@gmail.com>
2016-11-05 12:30:56 +0200

    new version

---
+++ polls/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Question
+
+from django.contrib import admin
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
